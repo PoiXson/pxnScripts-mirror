8712f4bdf42ace422af2e2328166760c5b8bdec0
Lop <lop@matt.poixson.com>
2019-09-05 19:02:50 -0400

    improvements to chmodr and chownr scripts

--- src/chmodr.sh
+++ src/chmodr.sh
@@ -32,26 +32,47 @@ if [ $# -lt 3 ]; then
 	echo
 	exit 1
 fi
+
+
+DIRS_PERM="$1" ; shift
+FILE_PERM="$1" ; shift
+if [ $# -gt 1 ]; then sStr='s' ; fi
 echo
-echo "Path: $3"
-echo "Setting dirs to:  $1"
-echo "Setting files to: $2"
+echo "Path${sStr}: $@"
+echo "Setting dirs to:  $DIRS_PERM"
+echo "Setting files to: $FILE_PERM"
 echo
-pushd "$3" || exit 1
+
+
+function do_chmodr() {
+pushd "$1" || exit 1
 	ENTRIES=(`ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }'`)
+	MAX_LEN=0
+	for ENTRY in "${ENTRIES[@]}"; do
+		LEN=$( echo -n "$ENTRY" | wc -m )
+		if [ $LEN -gt $MAX_LEN ]; then
+			MAX_LEN=$LEN
+		fi
+	done
 	for ENTRY in "${ENTRIES[@]}"; do
 		if [ ! -z $ENTRY ]; then
+			ENTRY_PAD=''
+			LEN=$( echo -n "$ENTRY" | wc -m )
+			PAD_LEN=$(( $MAX_LEN - $LEN ))
+			if [ $PAD_LEN -gt 0 ]; then
+				ENTRY_PAD=$(printf "%${PAD_LEN}s")
+			fi
 			if [ -h "$ENTRY" ]; then
 				echo "S: $ENTRY"
 			elif [ -d "$ENTRY" ]; then
-				echo "D: $ENTRY"
+				echo -n "D: ${ENTRY}${ENTRY_PAD}"
 				echo -n "  "
-				chmod -c "$1" "$ENTRY"
+				\chmod -c "$DIRS_PERM" "$ENTRY"
 				echo -ne "\r"
 			elif [ -f "$ENTRY" ]; then
-				echo "F: $ENTRY"
+				echo -n "F: ${ENTRY}${ENTRY_PAD}"
 				echo -n "  "
-				chmod -c "$2" "$ENTRY"
+				\chmod -c "$FILE_PERM" "$ENTRY"
 				echo -ne "\r"
 			else
 				echo "UNKNOWN: $ENTRY"
@@ -61,6 +82,14 @@ pushd "$3" || exit 1
 		fi
 	done
 popd
+}
+
+
+for P in "$@"; do
+	do_chmodr $P
+done
+
+
 echo
 echo "Finished!"
 echo
