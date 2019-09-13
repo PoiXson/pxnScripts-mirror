44fa57b10b0d851ad2fcbceb8ee28dee4ad70248
lorenzop <lorenzo@poixson.com>
2019-09-13 15:15:16 -0400

    added more 'cd ../..' aliases

--- src/aliases.sh
+++ src/aliases.sh
@@ -30,6 +30,12 @@ alias ~='cd ~'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias .2='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+alias .6='cd ../../../../../..'
 
 
 # exit/kill aliases
