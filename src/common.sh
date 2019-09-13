eb9677e6a845434c5dddea47e6cb0ff64049c50b
lorenzop <lorenzo@poixson.com>
2019-09-13 15:16:08 -0400

    added path_..() functions to common.sh script

--- src/common.sh
+++ src/common.sh
@@ -194,6 +194,12 @@ function is_locked() {
 
 
 
+path_append ()  { path_remove $1; export PATH="$PATH:$1"; }
+path_prepend () { path_remove $1; export PATH="$1:$PATH"; }
+path_remove ()  { export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
+
+
+
 # function valid_ip {
 #	local ip=$1
 #	local stat=1
