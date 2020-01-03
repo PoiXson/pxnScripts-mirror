3b64e1135992df5ab21cab45bf10c12c34af0d91
lorenzop <lorenzo@poixson.com>
2020-01-03 16:15:47 -0500

    fixed profile.sh script to only include common.sh and aliases.sh if existing

--- src/profile.sh
+++ src/profile.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 # profile.sh
 
-source /usr/bin/shellscripts/common.sh
-source /usr/bin/shellscripts/aliases.sh
+if [ -e /usr/bin/shellscripts/common.sh ]; then
+	source /usr/bin/shellscripts/common.sh
+fi
+
+if [ -e /usr/bin/shellscripts/aliases.sh ]; then
+	source /usr/bin/shellscripts/aliases.sh
+fi
