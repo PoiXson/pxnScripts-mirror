ff104a407178eb920c2a1951d2ed66bc871d6379
lorenzop <lorenzo@poixson.com>
2021-01-30 20:13:05 -0500

    moving progress_percent() function from progresspercent.sh to common.sh

--- src/common.sh
+++ src/common.sh
@@ -162,6 +162,29 @@ function sleepdotdot() {
 
 
 
+function progress_percent() {
+	let width=25
+	if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+		echo 'Missing required argument!'
+		exit 1
+	fi
+	TOTAL=$1
+	CURRENT=$2
+	let val=$CURRENT*$width
+	let val=$val/$TOTAL
+	echo -n '['
+	for ((i=1; $i<=$width; i++)); do
+		if [ $i -le $val ]; then
+			echo -n '='
+		else
+			echo -n ' '
+		fi
+	done
+	echo ']'
+}
+
+
+
 function get_lock() {
 	for i in {20..1} ; do
 		is_locked "$@"
