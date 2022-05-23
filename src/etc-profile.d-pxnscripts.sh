bb0442e49026d7da0370329ed07408dc6feda993
lorenzop <lorenzo@poixson.com>
2022-05-22 21:03:51 -0400

    fixed comments in etc-profile.d-pxnscripts.sh

--- src/etc-profile.d-pxnscripts.sh
+++ src/etc-profile.d-pxnscripts.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
-# profile.sh
+# /etc/profile.d/pxnscripts.sh
 
+
+# common
 if [ -e /usr/bin/pxn/scripts/common.sh ]; then
 	source /usr/bin/pxn/scripts/common.sh
 fi
 
+# aliases
 if [ -e /usr/bin/pxn/scripts/aliases.sh ]; then
 	source /usr/bin/pxn/scripts/aliases.sh
 fi
