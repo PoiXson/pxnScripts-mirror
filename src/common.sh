5ca0f2779b90f0c482fdcadd70caa4c0166d3357
lorenzop <lorenzo@poixson.com>
2022-05-24 02:21:33 -0400

    small fixes and cleanup to path_append() functions in common.sh

--- src/common.sh
+++ src/common.sh
@@ -48,9 +48,9 @@ fi
 
 
 
-path_append ()  { path_remove $1; export PATH="$PATH:$1"; }
-path_prepend () { path_remove $1; export PATH="$1:$PATH"; }
-path_remove ()  { export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
+function path_append()  { path_remove "$1" ; export PATH="$PATH:$1" ; }
+function path_prepend() { path_remove "$1" ; export PATH="$1:$PATH" ; }
+function path_remove()  { export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
 
 
 
