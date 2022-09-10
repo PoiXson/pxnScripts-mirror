26fe9fd574fb0c3e28b071a03fb200ca1174de32
lorenzop <lorenzo@poixson.com>
2022-09-10 15:19:20 -0400

    changing license from GPL3 to AGPL3

--- src/defines.sh
+++ src/defines.sh
@@ -1,22 +1,22 @@
 ##==============================================================================
-## Copyright (c) 2013-2022 PoiXson, Mattsoft
-## <https://poixson.com> <https://mattsoft.net>
-## Released under the GPL 3.0
+## Copyright (c) 2013-2022 Mattsoft/PoiXson
+## <https://mattsoft.net> <https://poixson.com>
+## Released under the AGPL 3.0
 ##
 ## Description: Common methods and utilities for pxn shell scripts.
 ##
 ## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
 ##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+## GNU Affero General Public License for more details.
 ##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##==============================================================================
 # defines.sh
 
