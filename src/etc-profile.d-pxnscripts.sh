650b29a489bba0526aebb5f570ab2499a6b5aa55
lorenzop <lorenzo@poixson.com>
2022-11-17 21:54:34 -0500

    added screen dir /home/user/.local/screen to etc/profile.d/pxnscripts.sh

--- src/etc-profile.d-pxnscripts.sh
+++ src/etc-profile.d-pxnscripts.sh
@@ -6,3 +6,13 @@
 if [ -e /usr/bin/pxn/scripts/common.sh ]; then
 	source /usr/bin/pxn/scripts/common.sh
 fi
+
+
+# screen
+if [[ ! -e "/home/$USER/.local/" ]]; then
+	\install  -m 0755  -d "/home/$USER/.local/"  || exit 1
+fi
+if [[ ! -e "/home/$USER/.local/screen/" ]]; then
+	\install  -m 0700  -d "/home/$USER/.local/screen/"  || exit 1
+fi
+export SCREENDIR=/home/$USER/.local/screen
