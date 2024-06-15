963a72a6e88574ce967a29e0e8dc8969035ea212
lorenzop <lorenzo@poixson.com>
2024-06-14 23:48:11 -0400

    updated script headers

--- src/aliases.sh
+++ src/aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 ##==============================================================================
-## Copyright (c) 2013-2022 Mattsoft/PoiXson
+## Copyright (c) 2013-2024 Mattsoft/PoiXson
 ## <https://mattsoft.net> <https://poixson.com>
 ## Released under the AGPL 3.0
 ##
--- src/defines.sh
+++ src/defines.sh
@@ -1,5 +1,5 @@
 ##==============================================================================
-## Copyright (c) 2013-2022 Mattsoft/PoiXson
+## Copyright (c) 2013-2024 Mattsoft/PoiXson
 ## <https://mattsoft.net> <https://poixson.com>
 ## Released under the AGPL 3.0
 ##
